b14f622de354c638941275bde83b1f3d0281578a
CyraMoss <cyramoss@icloud.com>
2023-01-18 11:45:57 +1300

    added store provider and cart badge

--- noiz/src/App.js
+++ noiz/src/App.js
@@ -4,17 +4,33 @@ import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import { useContext } from 'react';
+import { Store } from './Store';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <Router>
       <div className="d-flex flex-column site-container">
         <header>
-          <Navbar bg="dark" varient="dark">
+          <Navbar className="navbar-dark" bg="dark">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>NZ Noiz</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
