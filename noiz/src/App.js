059194088d68a021acf7a28f196f143d5fce5bf2
CyraMoss <cyramoss@icloud.com>
2023-01-17 21:21:15 +1300

    trying to add routes

--- noiz/src/App.js
+++ noiz/src/App.js
@@ -1,33 +1,21 @@
-import data from './data';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <a href="/">NZ Noiz</a>
-      </header>
-      <main>
-        <h1> Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}.00</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <a href="/">NZ Noiz</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
