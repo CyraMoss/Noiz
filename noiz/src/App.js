0d1dbbe96e2740248cba23967dd256f972d4f1b0
CyraMoss <cyramoss@icloud.com>
2023-01-18 00:25:42 +1300

    added bootstrap
    divided the screen with header main and footer

--- noiz/src/App.js
+++ noiz/src/App.js
@@ -1,21 +1,36 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to={'/'}>NZ Noiz</Link>
+          <Navbar bg="dark" varient="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>NZ Noiz</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
+
         <main>
-          <Routes>
-            <Route path="/product/:slug" exact element={<ProductScreen />} />
-            <Route path="/" exact element={<HomeScreen />} />
-          </Routes>
+          {' '}
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" exact element={<ProductScreen />} />
+              <Route path="/" exact element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </Router>
   );
